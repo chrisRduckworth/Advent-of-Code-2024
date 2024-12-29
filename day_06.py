cee9830f1ae3977b125598770f7c10e62915d426
chrisrduckworth <c_duckworth@hotmail.com>
2024-12-29 20:37:09 +0000

    refactor for speed from 25s to 1.5s

--- day_06.py
+++ day_06.py
@@ -1,94 +1,87 @@
-def count_pos(map):
-    height = len(map)
-    width = len(map[0])
-    pos = complex("".join(map).find("^") %
-                  width, "".join(map).find("^") // width)
+def create_dict(input):
+    out = {}
+    for y, l in enumerate(input):
+        for x, c in enumerate(l):
+            out[x + 1j * y] = c
+    return out
 
-    def replace_step(pos, m=map):
-        # replaces the character in the string with an X
-        x, y = int(pos.real), int(pos.imag)
-        m[y] = m[y][:x] + "X" + m[y][x+1:]
-        return
 
+def find_path(map):
+    start = [k for k, v in map.items() if v == "^"][0]
     dir = -1j
-    while 0 <= pos.real < width and 0 <= pos.imag < height:
-        if map[int(pos.imag)][int(pos.real)] == "#":
+    pos = start
+    path = []
+    while pos in map:
+        if pos+dir in map and map[pos + dir] == "#":
             # turn
-            pos -= dir
-            dir = dir * 1j
-            pass
+            dir *= 1j
         else:
-            replace_step(pos)
+            path.append((pos, dir))
             pos += dir
+    return path
 
-    return "".join(map).count("X")
+
+def part_1(input):
+    map = create_dict(input)
+    path = find_path(map)
+    return len(set(s[0] for s in path))
 
 
-# after attempting to be clever for a while, I just used brute force
+def find_loops(map, path):
+    # for each point on the path, replace it with an obstacle
+    # and check if it has created a loop
+    loops = set()
+    path_t = {p[0]: len(path) - 1 - i for i, p in enumerate(reversed(path))}
 
-def count_loops(map):
-    # lets just put an O in each space and see if it forms a loop
-    height = len(map)
-    width = len(map[0])
-    start = complex("".join(map).find("^") %
-                    width, "".join(map).find("^") // width)
-    loops = 0
-    # obstruction location is an integer because it's easier to increment
-    obs_loc = 0
+    for pos, index in path_t.items():
+        original_val = map[pos]
+        map[pos] = "#"
 
-    while obs_loc < width * height:
-        pos = start
-        dir = -1j
+        visited = set(path[:index])
+        is_loop = False
+        n_pos, n_dir = path[index - 1]
 
-        obs_x, obs_y = obs_loc % width, obs_loc // width
-        if map[obs_x][obs_y] == "#" or map[obs_x][obs_y] == "^":
-            # we can skip this space to save time
-            pass
-
-        cached_row = map[obs_y]
-        # add the obstruction
-        map[obs_y] = map[obs_y][:obs_x] + "#" + map[obs_y][obs_x+1:]
-
-        # test the obstruction to see if we find a loop
-        visited = set()
-        loop = False
         while True:
-            next_pos = pos + dir
-            n_x, n_y = int(next_pos.real), int(next_pos.imag)
-            if n_x < 0 or n_x >= width or n_y < 0 or n_y >= height:
-                # out of the grid, so no loop
+            next_pos = n_pos + n_dir
+            if next_pos not in map:
+                # we've left, so no loop
                 break
-            elif map[n_y][n_x] == "#":
-                # about to hit an obstacle, so turn
-                dir *= 1j
+            elif map[next_pos] == "#":
+                # turn
+                n_dir *= 1j
             else:
-                # take a step
-                pos = next_pos
+                # straight forward
+                n_pos = next_pos
 
-            if (pos, dir) in visited:
-                # formed a loop
-                loop = True
+            if (n_pos, n_dir) in visited:
+                # found a loop
+                is_loop = True
                 break
 
-            visited.add((pos, dir))
+            visited.add((n_pos, n_dir))
 
-        if loop:
-            loops += 1
-        obs_loc += 1
+        if is_loop:
+            # since each point on the path can be approached
+            # from multiple direction, we use a set only including
+            # the position
+            loops.add(pos)
 
-        map[obs_y] = cached_row
+        map[pos] = original_val
 
     return loops
 
 
+def part_2(input):
+    map = create_dict(input)
+    path = find_path(map)
+    loops = find_loops(map, path)
+    return len(loops)
+
+
 if __name__ == "__main__":
     with open("inputs/day_06.txt") as f:
-        map = [l.strip() for l in f.readlines()]
-        steps = count_pos(map)
-        print(steps, "< steps")
-    
-    # need to reread the file because part 1 wasn't pure (oops)
-    with open("inputs/day_06.txt") as f:
-        map = [l.strip() for l in f.readlines()]
-        obs = count_loops(map)
-        print(obs, "< obs")
+        input = [l.strip() for l in f.readlines()]
+        positions = part_1(input)
+        print(positions, "< part 1")
+        loop_count = part_2(input)
+        print(loop_count, "< part 2")
